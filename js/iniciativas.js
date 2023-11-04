973b1d7ebcf7096bfa9a0c0f1896f8fc5f105150
Daniela <dggtn9@gmail.com>
2023-11-03 21:19:32 -0300

    cambios

--- js/iniciativas.js
+++ js/iniciativas.js
@@ -9,9 +9,15 @@ function siguientePaso() {
     pasoSiguiente.style.display = "block"
 }
 
-function donacionexitosa(){
+function donacionExitosa(){
     alert("Tu donación se ha realizado con éxito ¡Gracias por ser parte de Share Before Waste!")
     let paso = document.getElementById("paso3")
     paso.style.display = "none";
+    reiniciarFormulario();
+}
 
+function reiniciarFormulario(){
+    pasoActual=1;
+    let paso = document.getElementById("paso" + pasoActual)
+    paso.style.display = "block";
 }
