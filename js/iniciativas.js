6e0c2264cb23efa21db0c3ccb6a96d049a3d2ac3
Daniela <dggtn9@gmail.com>
2023-11-04 23:18:22 -0300

    botones javascript

--- js/iniciativas.js
+++ js/iniciativas.js
@@ -1,7 +1,50 @@
+window.onload=cargar;
+//Variables globales
 let pasoActual = 1;
 let pasosTotales = 3;
+let montoADonar =0;
+
+//Dentro de cada función hay variables locales, no las puedo usar en otra función
+
+function cargar(){
+   let boton1000 = document.getElementById("btnradio1")
+   boton1000.onchange= seleccionar1000;
+   let boton1500 = document.getElementById("btnradio2")
+   boton1500.onchange= seleccionar1500;
+   let boton2000 = document.getElementById("btnradio3")
+   boton2000.onchange= seleccionar2000;
+   let botonOtroMonto = document.getElementById("btnradio4")
+   botonOtroMonto.onchange= seleccionarOtroMonto;
+
+   
+   
+}
+
+function seleccionar1000(){
+    montoADonar=1000
+    let botonOtroMonto = document.getElementById("otroMon")
+    botonOtroMonto.style.display= "none";
+
+}
+function seleccionar1500(){
+    montoADonar=1500
+    let botonOtroMonto = document.getElementById("otroMonto")
+    botonOtroMonto.style.display= "none";
+
+}
+function seleccionar2000(){
+    montoADonar=2000
+    let botonOtroMonto = document.getElementById("otroMonto")
+    botonOtroMonto.style.display= "none";
+
+}
+function seleccionarOtroMonto(){
+    montoADonar=2000
+    let botonOtroMonto = document.getElementById("otroMonto")
+    botonOtroMonto.style.display= "block";
 
 
+}
 function siguientePaso() {
     let paso = document.getElementById("paso" + pasoActual)
     paso.style.display = "none";
@@ -34,4 +77,9 @@ function reiniciarFormulario(){
     pasoActual=1;
     let paso = document.getElementById("paso" + pasoActual)
     paso.style.display = "block";
+}
+
+function seleccionarMontoADonar(){
+
+
 }
